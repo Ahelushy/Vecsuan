f0a7d6d29457c0be4b1f62e07205a561b91d8b45
火紅之葉 <Web_13234@outlook.com>
2023-03-09 14:05:28 +0800

    Create InputHandler

---
+++ Program/InputHandler.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Godot;
+
+namespace Vecsuan.Program;
+
+internal partial class InputHandler : Node
+{
+    public override void _UnhandledInput(InputEvent @event)
+    {
+        
+    }
+}
