918c2cfa71e85b60fbe50e5cc920477ff1687c10
火紅之葉 <Web_13234@outlook.com>
2023-03-09 21:37:52 +0800

    左击绘制点
    新增点对象、左击响应

---
+++ Element/Point.cs
@@ -0,0 +1,32 @@
+﻿using Godot;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection.Metadata.Ecma335;
+
+namespace Vecsuan.Element
+{
+    public partial class Point : Node2D
+    {
+        //工厂
+        private Point() { }
+        private static readonly PackedScene Scene = GD.Load<PackedScene>("res://Element/Point.tscn");
+        public static Point NewInstance() => Scene.Instantiate<Point>();
+        public static Point NewInstance(Vector2 position)
+        {
+            var resultInstance = NewInstance();
+            resultInstance.Position = position;
+            return resultInstance;
+        }
+
+        //显示
+        [Export]
+        private Color Color { get; set; }
+        public override void _Draw()
+        {
+            DrawCircle(Vector2.Zero, 10, Color);
+        }
+    }
+}
